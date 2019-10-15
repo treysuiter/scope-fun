4880f659c17bed6a2a9dfbc2bbbd766b02afa88d
Trey Suiter <trey.suiter@gmail.com>
2019-10-15 10:23:54 -0500

    corrected index varialble

---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+console.log("Get'n all woo hoo")
+
+const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+// let y = 1
+
+for (let x = 1; x < cookies.length; x++) {
+    const currentCookie = cookies[x]
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+}
