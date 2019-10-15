3d7acfff6b84f70e72a0b71c17e8e4ffe4e6e218
Trey Suiter <trey.suiter@gmail.com>
2019-10-15 10:29:55 -0500

    added return statement to func

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,17 @@
-console.log("Get'n all woo hoo")
+// console.log("Get'n all woo hoo")
 
-const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
-// let y = 1
+// const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+// // let y = 1
 
-for (let x = 1; x < cookies.length; x++) {
-    const currentCookie = cookies[x]
-    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// for (let x = 1; x < cookies.length; x++) {
+//     const currentCookie = cookies[x]
+//     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// }
+
+const conjunction = function (firstWord, secondWord) {
+    const conjoinedWord = `${firstWord} ${secondWord}`
+    return conjoinedWord
 }
+
+console.log(conjunction("Master", "Card"))
+// console.log(conjoinedWord)
