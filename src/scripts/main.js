5e41cf8f96dcaebd437099777db9a748acf25c25
Trey Suiter <trey.suiter@gmail.com>
2019-10-15 10:56:54 -0500

    fixed all kinds of messed up stuff

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,10 +8,28 @@
 //     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
 // }
 
-const conjunction = function (firstWord, secondWord) {
-    const conjoinedWord = `${firstWord} ${secondWord}`
-    return conjoinedWord
+// const conjunction = function (firstWord, secondWord) {
+//     const conjoinedWord = `${firstWord} ${secondWord}`
+//     return conjoinedWord
+// }
+
+// console.log(conjunction("Master", "Card"))
+// console.log(conjoinedWord)
+
+
+const ModSquad = {
+    "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+        "series": {
+        "start": "1968",
+        "end": "1973"
+    }
 }
 
-console.log(conjunction("Master", "Card"))
-// console.log(conjoinedWord)
+let HTMLRepresentation = `<h1>The Mod Squad</h1>`
+
+ModSquad.members.forEach (member => {
+    HTMLRepresentation += `<div>${member}</div>`
+})
+
+
+document.querySelector(".show-info").innerHTML = HTMLRepresentation
