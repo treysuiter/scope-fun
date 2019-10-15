4552243c44e3184e15dae9bc97d650492d5979cf
Trey Suiter <trey.suiter@gmail.com>
2019-10-15 12:00:05 -0500

    removed un code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -69,7 +69,7 @@ const llamaNamer = function () {
     const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
     const randomizer = Math.floor(Math.random() * 7)
 
-    const namer = function a() {
+    const namer = function () {
         const suffix = " the Llama"
         const name = possibleNames[randomizer]
         return name + suffix
